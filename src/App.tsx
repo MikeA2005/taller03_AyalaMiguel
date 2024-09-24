811ed4d8cdb21e228b1320642ce150209a1a0dae
Romero <sromeroar20@gmail.com>
2024-09-24 12:24:07 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import miImagen from './assets/img/cr7.jpg'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={miImagen} alt="Cristiano Ronaldo" width={500} />
+        <h1>Cristiano Ronaldo</h1>
+        <p>Cristiano Ronaldo, uno de los futbolistas más icónicos de todos los tiempos, dejó una huella imborrable en el Real Madrid. Llegó al club en 2009 procedente del Manchester United, y rápidamente se convirtió en el pilar del equipo. Durante su estancia en el Santiago Bernabéu, Ronaldo no solo rompió récords, sino que también redefinió lo que significa ser un delantero moderno.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
